a3729eb676c33ad586da152d8d0b4e69e1d6f919
fdoclaveros <jc.rueger@gmail.com>
2024-02-12 13:06:18 -0300

    hola

--- 03Python/Semana2/Dia06/Ejercicio2.py
+++ 03Python/Semana2/Dia06/Ejercicio2.py
@@ -3,21 +3,48 @@ agenda = {}
 
 # Variable para controlar si el usuario desea salir del programa
 salir = False
+nombreCorrecto = False
+numeroCorrecto = False
 
 # Mensaje de bienvenida
 print("**** Bienvenido a AGENDA ****\n")
 
-# Bucle principal que permite al usuario agregar contactos a la agenda
+def validar_input(texto):
+    """
+    Valida si el input es una cadena de texto sin números.
+    :param texto: El input a validar.
+    :return: True si es válido (solo letras), False si no lo es.
+    """
+    return texto.isalpha()
+
 while (not salir):
-    # Solicita al usuario que ingrese un nombre y un teléfono
-    nombre = input("Ingrese un nombre:\n")
-    telefono = int(input("Ingrese un telefono:\n"))
+
+    # Bucle principal que permite al usuario agregar contactos a la agenda
+    while (not nombreCorrecto):
+
+        nombre = input("Ingresa un nombre: ")
+        verificandonombre = validar_input(nombre)
+
+        if verificandonombre == True:
+            print("nombre valido")
+            nombreCorrecto = True
+        else:
+            print("ingrese un nombre sin numeros")
+
+    while (not numeroCorrecto):
+        try:
+            telefono = int(input("Ingrese un telefono:\n"))
+            numeroCorrecto = True
+        except ValueError:
+            print("ingrese un nombre sin numeros")
 
     # Verifica si el nombre ya existe en la agenda
     if nombre not in agenda:
         # Si el nombre no está en la agenda, lo agrega junto con su teléfono
         agenda[nombre] = telefono
         print('Contacto guardado exitosamente!!')
+        nombreCorrecto = False
+        numeroCorrecto = False
     else:
         # Si el nombre ya está en la agenda, muestra un mensaje indicando que ya existe
         print('El nombre ya se encuentra en la agenda!!')
